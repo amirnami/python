5028cff4be0583b046e57a71e9693affb8a199e7
amirnami <amir_ahmadinami@outlook.com>
2017-04-07 18:02:33 +0430

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,9 +1,9 @@
 import requests
 import math
 
-# Get a copy of the default headers 
+# Changing User-Agent 
 
-header = requests.utils.default_headers()
+header = requests.utils.default_headers() # Get a copy of the default headers 
 header.update(
     {
            "User-Agent" : "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
