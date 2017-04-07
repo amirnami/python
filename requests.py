de8a108027bd025e610053dda4386e08ef7227b5
amirnami <amir_ahmadinami@outlook.com>
2017-04-07 17:17:07 +0430

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,9 +1,13 @@
 import requests
 import math
 
+# Displaying the Weather with Python 
+
 url = "http://api.openweathermap.org/data/2.5/weather"
 query_string = {'q' : 'Tehran,ir', 'appid' : '61f18264b1a8919cea3d08556ebe74f8'}
 response = requests.get(url, params=query_string)
 temp =  int(response.json()['main']['temp'])
 temp = int(math.ceil(temp - 273.15))
 print temp
+
+# Displaying the Weather with Python 
